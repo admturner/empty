2e9b417adee4555bfb93c0fc6dedbc5a48fb0fd0
Adam Turner <support@adamturner.org>
2019-06-26 21:16:36 -0700

    Add template function to conditionally add body classes

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -5,6 +5,7 @@
  * @package Bream
  * @since 0.4.0
  */
+
 namespace Bream\Includes\TemplateFunctions;
 
 /**
@@ -13,11 +14,35 @@ namespace Bream\Includes\TemplateFunctions;
  * @since 0.4.0
  *
  * @param string $stylesheet_dir_uri Stylesheet directory URI.
- * @param string $stylesheet         Name of the activated theme's directory.
- * @param string $theme_root_uri     The theme root URI.
  * @return string The path to the stylesheet directory.
  */
 function adjust_stylesheet_directory_uri( $stylesheet_dir_uri ) {
-	return $stylesheet_dir_uri . '/build/css';
+	return $stylesheet_dir_uri . '/build';
 }
 add_filter( 'stylesheet_directory_uri', __NAMESPACE__ . '\adjust_stylesheet_directory_uri', 10, 1 );
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @since 0.4.0
+ *
+ * @param string[] $classes Classes for the body element.
+ * @return string[] The modifed body element classes array.
+ */
+function body_classes( $classes ) {
+	// Adds a class of hfeed to non-singular pages.
+	if ( ! is_singular() ) {
+		$classes[] = 'hfeed';
+	}
+
+	// Adds classes to indicate whether sidebars are present.
+	if ( is_active_sidebar( 'sidebar-main' ) ) {
+		$classes[] = 'has-sidebar-main';
+	}
+	if ( is_active_sidebar( 'sidebar-footer' ) ) {
+		$classes[] = 'has-sidebar-footer';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', __NAMESPACE__ . '\body_classes' );
